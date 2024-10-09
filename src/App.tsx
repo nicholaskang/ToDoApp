4c37633d9a4ac89b4e3402f2ed39213e786278da
Nick Kang <nickkang257@gmail.com>
2024-10-08 18:29:09 -0700

    Adds styles to InputField

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,6 @@ import InputField from "./components/InputField";
 
 const App: React.FC = () => {
 
-  // let name: string = 'Nick Kang';
-  // let age: number = 35;
-  // let age: number | string = 35;
-  // age = 'thirty five';
-  // let isStudent: boolean = true;
-  // let hobbies: string[] = ['tennis', 'reading', 'travel'];
-  // let role: [number, string];
-  // role = [5, 'test'];
-  // function printName(name: string) {
-  //   console.log(name)
-  // }
-  // const printName = (name: string):void => {
-  //   console.log(name);
-  // }
-  // type Person = {
-  //   name: string;
-  //   age?: number;
-  // }
-  // interface Person {
-  //   name: string;
-  //   age?: number;
-  // }
-  // interface Guy extends Person {
-  //   profession: string;
-  //   yearsExperience?: number;
-  // }
-  // let person: Person = {
-  //   name: 'Nicholas',
-  //   // age: 40
-  // }
-  // printName(person.name);
-  // printName('Nick');
-  // let people: Person[] = {}
 
   return (
     <>
@@ -42,7 +9,7 @@ const App: React.FC = () => {
         <header className="flex justify-center p-6 z-10">
           <h1 className="text-3xl text-white">Taskify</h1>
         </header>
-        <section className="flex justify-center">
+        <section className="flex justify-center mt-4">
           <InputField />
         </section>
       </main>
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,9 @@
 const InputField = () => {
   return (
-    <form className="flex items-center">
-      <label className="mr-2 text-slate-200">Search Task:</label>
-      <input type="input" placeholder="Enter a task"/>
-      
+    <form className="flex flex-col items-start w-60">
+      <label className="mb-1 text-slate-200">Add a Task:</label>
+      <input className="w-full p-2 mb-2" type="input" placeholder="Enter task name"/>
+      <button className="w-full text-center text-white rounded-md p-2 bg-sky-800 hover:bg-sky-900 active:bg-sky-950" type="submit">Add Task</button>
     </form>
   )
 }
