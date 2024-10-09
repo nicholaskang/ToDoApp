e987fc968d3374e2ba2ee27ed71d990b610876f1
Nick Kang <nickkang257@gmail.com>
2024-10-08 17:12:22 -0700

    Buids header and starts search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,51 @@
+import InputField from "./components/InputField";
 
+const App: React.FC = () => {
 
-function App() {
+  // let name: string = 'Nick Kang';
+  // let age: number = 35;
+  // let age: number | string = 35;
+  // age = 'thirty five';
+  // let isStudent: boolean = true;
+  // let hobbies: string[] = ['tennis', 'reading', 'travel'];
+  // let role: [number, string];
+  // role = [5, 'test'];
+  // function printName(name: string) {
+  //   console.log(name)
+  // }
+  // const printName = (name: string):void => {
+  //   console.log(name);
+  // }
+  // type Person = {
+  //   name: string;
+  //   age?: number;
+  // }
+  // interface Person {
+  //   name: string;
+  //   age?: number;
+  // }
+  // interface Guy extends Person {
+  //   profession: string;
+  //   yearsExperience?: number;
+  // }
+  // let person: Person = {
+  //   name: 'Nicholas',
+  //   // age: 40
+  // }
+  // printName(person.name);
+  // printName('Nick');
+  // let people: Person[] = {}
 
   return (
     <>
-      <p className="m-6 text-xl">Hello World</p>
+      <main className="bg-sky-600 h-screen">
+        <header className="flex justify-center p-6 z-10">
+          <h1 className="text-3xl text-white">Taskify</h1>
+        </header>
+        <section className="flex justify-center">
+          <InputField />
+        </section>
+      </main>
     </>
   )
 }
---
+++ src/components/InputField.tsx
@@ -0,0 +1,11 @@
+const InputField = () => {
+  return (
+    <form className="flex items-center">
+      <label className="mr-2 text-slate-200">Search Task:</label>
+      <input type="input" placeholder="Enter a task"/>
+      
+    </form>
+  )
+}
+
+export default InputField
